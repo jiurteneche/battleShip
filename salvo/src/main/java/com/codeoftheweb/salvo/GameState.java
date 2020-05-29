07a46e9b74b54f7ba11b1c00691dad584fffaefe
jiurteneche <jiurteneche@gmail.com>
2020-05-29 16:01:23 -0300

    Falta el front de la última actividad

---
+++ salvo/src/main/java/com/codeoftheweb/salvo/GameState.java
@@ -0,0 +1,14 @@
+package com.codeoftheweb.salvo;
+
+public enum  GameState {
+
+        wait_an_opponent,
+        place_ships,
+        wait_opponent_ships,
+        fire_salva,
+        wait_opponent_salva,
+        win,
+        tie,
+        lose,
+
+}
